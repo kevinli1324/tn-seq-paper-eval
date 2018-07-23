78cf7fb142a3fadbdb2958706c062ebc781a20c2
Kevin Li <kevin.li1324@gmail.com>
2018-07-23 10:51:44 -0400

    correlated simulations

--- run_count_simulation.R
+++ run_count_simulation.R
@@ -3,7 +3,7 @@ source("pca_functions.R")
 source("t_sim_functions.R")
 ################################# GENERATE DATA ###################################################
 #generate simulations list6
-simulation_object <- cor_wrapper(num_clusters = 30, n_experiments = 162, max_group = 5, prop_affec =1, mean0_vec = real_counts$set1IT002.Time0)
+simulation_object <- cor_wrapper(num_clusters = 30, n_experiments = 162, max_group = 5, prop_affec = .5, mean0_vec = real_counts$set1IT002.Time0)
 #t_matrix
 t_matrix <- simulation_object$t_matrix
 #logfit matirxx
@@ -18,14 +18,21 @@ fit_vec <- as.vector(simulation_object$fit)
 mix_vec <- as.vector(simulation_object$mixing)
 #controls 
 mean0 <- as.vector(simulation_object$init)
-
 #pca mess
 pca_object <- prcomp(logfit_matrix)
-princomps <-  which(pca_object$sdev^2/sum(pca_object$sdev^2) < .025)[1]
+princomps <-  which(pca_object$sdev^2/sum(pca_object$sdev^2) < .01)[1]
 
-stan_object <- pca_wrapper(data = logfit_matrix, princomps = 2, clusters = princomps,  method = "kmeans", file = "simple_multivar.stan")
+stan_object <- pca_wrapper(data = logfit_matrix, princomps = 5, clusters = princomps,  method = "kmeans", file = "simple_multivar.stan", optimize = F)
 #stan_count <- pca_wrapper(data = count_matrix, princomps = princomps, mexthod = "kmeans", file = "pois_multivar.stan", mean0 = mean0)
 mclust_list <- apply(logfit_matrix, MARGIN = 1, Mclust, G = 2, modelNames = "E") 
+
+
+# aij range calculation/ Bayesian confidence intervals
+combined_frame <- combine_princomps(stan_object$models, stan_object$assign)
+aij_dist <- matrix(0, nrow = nrow(logfit_matrix), ncol = 1000) 
+store <- get_aij(logfit_matrix[1,1], combined_frame, 1 )
+
+hist(get_aij(logfit_matrix[3,10], combined_frame, 1))
 
 #calculate aij matrices 
 stan_aij <- matrix(0, nrow = nrow(logfit_matrix), ncol  = ncol(logfit_matrix))
@@ -34,7 +41,7 @@ rank_aij <- matrix(0, nrow = nrow(logfit_matrix), ncol = ncol(logfit_matrix))
 t_aij <- matrix(0, nrow = nrow(logfit_matrix), ncol = ncol(logfit_matrix))
 
 for(i in 1:nrow(logfit_matrix)) {
-  stan_aij[i,] <- alpha_wrap( extract_fix_param(stan_object$means,i), logfit_matrix[i,], additive = F)
+  stan_aij[i,] <- alpha_wrap( extract_fix_param(stan_object$means,i), logfit_matrix[i,], additive = T)
   
   if(as.numeric(Mclust(logfit_matrix[i,], G = 1, modelNames = "E")$BIC) - 12 > as.numeric(mclust_list[[i]]$BIC)) {
     mclust_aij[i,] <- rep(1, ncol(logfit_matrix))
@@ -94,7 +101,7 @@ summary_false <- summarise(group_by(master_class, type), mean(false_positive))
 #classification distributione
 class_plot <- ggplot(data = filter(plot_class, variable == "raw"), aes(value, fill = type)) + geom_histogram() + facet_grid(.~ type)
 
-ggplot(data = filter(plot_class, variable == "raw"), aes(y = value, x = type)) + geom_boxplot() + xlab("Classifier") + ylab("Classification Rate") + ggtitle("Classification Rate for Classifiers")
+ggplot(data = filter(plot_class, variable == "raw"), aes(y = value, x = type, fill= type)) + geom_boxplot() + xlab("Classifier") + ylab("Classification Rate") + ggtitle("Classification Rate for Classifiers")
 
 
 class_fit <- ggplot(data = filter(plot_class, variable == "raw"), aes(x = fit_ratio, y = value, color = type)) + geom_point() + facet_grid(.~ type)
@@ -106,18 +113,20 @@ entropy_fit <- ggplot(data = filter(plot_entropy), aes(x = fit_ratio, y = value,
 ggplot(data = filter(plot_entropy), aes(x = mix, y = value, color = type)) + geom_point() + facet_grid(.~ type)
 ggplot(data = filter(plot_entropy), aes(x = mean0, y = value, color = type)) + geom_point() + facet_grid(.~ type)
 
+ggplot(data = plot_entropy, aes(x = type, y = value)) + geom_boxplot() + xlab("Classifier") + ylab("Cross Entropy") + ggtitle("Cross Entropy of Classifications")
 
-ggplot(data = filter(plot_class, variable == "false_positive"), aes(y = value, x = type)) + geom_boxplot() + xlab("Classifier") + ylab("FDR") + ggtitle("FDR for Classifiers")
+ggplot(data = filter(plot_class, variable == "false_positive"), aes(y = value, x = type, fill = type)) + geom_boxplot() + xlab("Classifier") + ylab("FDR") + ggtitle("FDR for Classifiers")
 
-ggplot(data = filter(plot_class, variable == "pos_class"), aes(y = value, x = type)) + geom_boxplot() + xlab("Classifier") + ylab("FDR") + ggtitle("FDR for Classifiers")
-ggplot(data = filter(plot_entropy), aes(y = value, x = type)) + geom_boxplot() + xlab("Classifier") + ylab("Entropy") + ggtitle("Cross Entropy for Classifiers")
+ggplot(data = filter(plot_class, variable == "pos_class"), aes(y = value, x = type, fill = type)) + geom_boxplot() + xlab("Classifier") + ylab("FDR") + ggtitle("Positive Classification Rate")
+ggplot(data = filter(plot_entropy), aes(y = value, x = type, fill = type)) + geom_boxplot() + xlab("Classifier") + ylab("Entropy") + ggtitle("Cross Entropy for Classifiers")
 
 ## view
-extractwhich(stan_metrics$raw == 0)
+which(stan_metrics$raw < .2)
 #look ast posterior
-extract_plots_testparam(14, stan_object$means, logfit_matrix)
-draws <-  as.data.frame(stan_object$models[[4]])
+extract_plots_testparam(54, stan_object$means, logfit_matrix)
+draws <-  as.data.frame(stan_object$models[[5]])
 
-plot(draws$`mu0[1]`, draws$`mu1[1]`)
-
+plot(draws$`mu0[35]`, draws$`mu1[35]`, xlab = "mu0 posterior draws", ylab = "mu1 posterior draws", main = "mu1 and mu0 Posterior Draws")
+hist(draws$`theta[35]`)
+hist(draws$'theta[42]')
 which(stan_)
